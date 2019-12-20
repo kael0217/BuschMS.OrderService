1dd1ad2e05eef459a1516f91364204b7d2027fc9
kael0217 <kael.zhang@Wenyangs-MacBook-Pro-2.local>
2019-12-20 09:22:09 -0600

    first commit

---
+++ src/test/java/com/java/BuschMS/OrderService/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.BuschMS.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
